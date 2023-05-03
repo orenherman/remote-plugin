273f531ec5a31fb2c27b6bc40f60f0619b8a6d11
Oren Herman <oren.herman@torq.io>
2023-05-03 20:28:27 +0300

    remove redundant code

--- shared/interface.go
+++ shared/interface.go
@@ -6,7 +6,6 @@ package shared
 
 import (
 	"context"
-	"net/rpc"
 
 	"google.golang.org/grpc"
 
@@ -25,7 +24,6 @@ var Handshake = plugin.HandshakeConfig{
 // PluginMap is the map of plugins we can dispense.
 var PluginMap = map[string]plugin.Plugin{
 	"kv_grpc": &KVGRPCPlugin{},
-	"kv":      &KVPlugin{},
 }
 
 // KV is the interface that we're exposing as a plugin.
@@ -39,14 +37,6 @@ type KVPlugin struct {
 	// Concrete implementation, written in Go. This is only used for plugins
 	// that are written in Go.
 	Impl KV
-}
-
-func (p *KVPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
-	return &RPCServer{Impl: p.Impl}, nil
-}
-
-func (*KVPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
-	return &RPCClient{client: c}, nil
 }
 
 // This is the implementation of plugin.GRPCPlugin so we can serve/consume this.
